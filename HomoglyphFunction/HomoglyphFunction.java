f3071059e723994a2e6f8bc21bb1a268b52038fd
James Bostock <James.Bostock@checkmarx.com>
2022-06-17 14:36:31 +1000

    Add homoglyph example code
    Copied from https://github.com/nickboucher/trojan-source

---
+++ HomoglyphFunction/HomoglyphFunction.java
@@ -0,0 +1,15 @@
+// See https://github.com/nickboucher/trojan-source
+public class HomoglyphFunction
+{
+    public static void sаyHello() {
+        System.out.println("Hello, Worled!");
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello, Worled!");
+    }
+
+    public static void main(String[] args) {
+        sаyHello();
+    }
+}
