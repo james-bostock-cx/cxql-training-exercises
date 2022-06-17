448839283080a200349c11e53bd81a6cb7afb327
James Bostock <James.Bostock@checkmarx.com>
2022-06-17 14:36:31 +1000

    Add homoglyph example code

---
+++ HomoglyphFunction/HomoglyphFunction.java
@@ -0,0 +1,14 @@
+public class HomoglyphFunction
+{
+    public static void sаyHello() {
+        System.out.println("Hello, Worled!");
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello, Worled!");
+    }
+
+    public static void main(String[] args) {
+        sаyHello();
+    }
+}
